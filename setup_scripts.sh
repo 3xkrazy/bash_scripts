3b4ec43342bb70053dd3f3c41d7583d0e86d1f3b
3xkrazy <hanhle.psvita@gmail.com>
2016-06-25 17:28:44 +0000

    Update setup_scripts.sh

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -3,32 +3,23 @@
 
 dir_bin=~/.bin/
 
-corbenik_url=https://raw.githubusercontent.com/3xkrazy/3ds/master/scripts/corbenik.sh;
-nandtool_url=https://raw.githubusercontent.com/3xkrazy/3ds/master/scripts/nandtool.sh;
-funkeycia_url=https://raw.githubusercontent.com/3xkrazy/3ds/master/scripts/funkeycia.sh;
-funkeycia=https://raw.githubusercontent.com/llakssz/FunKeyCIA/master/FunKeyCIA.py;
-setaudio_url=https://raw.githubusercontent.com/3xkrazy/bash_scripts/master/system/setaudio.sh;
-pmove_url=https://raw.githubusercontent.com/3xkrazy/bash_scripts/master/system/pmove.sh;
+url_homebrew=https://raw.githubusercontent.com/3xkrazy/3ds/master/homebrew.sh;
+url_funkeycia=https://raw.githubusercontent.com/llakssz/FunKeyCIA/master/FunKeyCIA.py;
+url_setaudio=https://raw.githubusercontent.com/3xkrazy/bash_scripts/master/system/setaudio.sh;
+url_pmove=https://raw.githubusercontent.com/3xkrazy/bash_scripts/master/system/pmove.sh;
 
-rm -v $dir_bin/corbenik.sh;
-rm -v $dir_bin/nandtool.sh;
-rm -v $dir_bin/funkeycia.sh;
+rm -v $dir_bin/homebrew.sh;
 rm -v $dir_bin/FunKeyCIA.py;
 rm -v $dir_bin/setaudio.sh;
 rm -v $dir_bin/pmove.sh;
 
 cd $dir_bin;
+wget $url_homebrew;
+wget $url_funkeycia;
+wget $url_setaudio;
+wget $url_pmove;
 
-wget $corbenik_url;
-wget $nandtool_url;
-wget $funkeycia_url;
-wget $funkeycia;
-wget $setaudio_url;
-wget $pmove_url;
-
-chmod -v u+x $dir_bin/corbenik.sh;
-chmod -v u+x $dir_bin/nandtool.sh;
-chmod -v u+x $dir_bin/funkeycia.sh;
+chmod -v u+x $dir_bin/homebrew.sh;
 chmod -v u+x $dir_bin/FunKeyCIA.py;
 chmod -v u+x $dir_bin/setaudio.sh;
 chmod -v u+x $dir_bin/pmove.sh;
