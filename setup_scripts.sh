ac226db80b3ae19906644ba5547779f8656a39a6
3xkrazy <hanhle.psvita@gmail.com>
2016-06-27 01:34:59 +0000

    Update setup_scripts.sh

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -5,21 +5,25 @@ dir_bin=~/.bin/
 
 url_homebrew=https://raw.githubusercontent.com/3xkrazy/3ds/master/homebrew.sh;
 url_funkeycia=https://raw.githubusercontent.com/llakssz/FunKeyCIA/master/FunKeyCIA.py;
+url_3dsconv=https://raw.githubusercontent.com/ihaveamac/3dsconv/master/3dsconv.py;
 url_setaudio=https://raw.githubusercontent.com/3xkrazy/bash_scripts/master/system/setaudio.sh;
 url_pmove=https://raw.githubusercontent.com/3xkrazy/bash_scripts/master/system/pmove.sh;
 
 rm -v $dir_bin/homebrew.sh;
 rm -v $dir_bin/FunKeyCIA.py;
+rm -v $dir_bin/3dsconv.py;
 rm -v $dir_bin/setaudio.sh;
 rm -v $dir_bin/pmove.sh;
 
 cd $dir_bin;
 wget $url_homebrew;
 wget $url_funkeycia;
+wget $url_3dsconv;
 wget $url_setaudio;
 wget $url_pmove;
 
 chmod -v u+x $dir_bin/homebrew.sh;
 chmod -v u+x $dir_bin/FunKeyCIA.py;
+chmod -v u+x $dir_bin/3dsconv.py;
 chmod -v u+x $dir_bin/setaudio.sh;
 chmod -v u+x $dir_bin/pmove.sh;
